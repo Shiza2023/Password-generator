f2aa6fbc6482a399d133a6b6c46c0c4be82daf39
Shiza2023 <151557985+Shiza2023@users.noreply.github.com>
2023-11-20 21:02:07 +0300

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from random import *
 
 def generatr_password():
     chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for i in "il1Lo0O":
+        chars = chars.replace(i, "")
+
     print("Я генероатор паролей")
+
     kol = int(input("Введите количество паролей: "))
 
     for _ in range(kol):
